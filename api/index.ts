5e8bd32dcacd1bf0e3624f8367b1d1316aafc24b
‪Hazem Gamal Kamal <161766768+hazemio@users.noreply.github.com>
2026-08-25 18:48:57 +0300

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,12 @@
 import { createApp } from '../src/main';
 
 export default async function handler(req: any, res: any) {
+  // الرد الفوري على طلبات الـ Preflight
+  if (req.method === 'OPTIONS') {
+    res.status(204).end();
+    return;
+  }
+
   const app = await createApp();
   const instance = app.getHttpAdapter().getInstance();
   return instance(req, res);
